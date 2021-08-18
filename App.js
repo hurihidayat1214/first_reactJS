cf96a32c068d97798f6c1af94d3b6c610936e035
hurihidayat1214 <66757343+hurihidayat1214@users.noreply.github.com>
2021-08-18 11:55:57 +0700

    baru
    ini adalah first react js

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Edit di path <code>src/App.js</code> and save to reload.</p>
+        <p>Bismilah Semoga Semoga Tersemogakan</p>
+        <p>Kamu Pasti Bisa Menjadi Orang Yang Hebat Tetapi Ingatlah Keluarga mu dan Allah Jangan lah Kamu merasa Sombong</p>
+        <a
+          className="App-link"
+          href="https://kedaburapat.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
